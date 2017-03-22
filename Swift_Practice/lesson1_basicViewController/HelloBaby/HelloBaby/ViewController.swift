a719c3ec34bcc48448c8344130a3d7367a3eb41f
Coody <coodydeveloper@gmail.com>
2017-03-23 00:25:49 +0800

    [.feature ] Lesson 1 的程式碼

--- Swift_Practice/lesson1_basicViewController/HelloBaby/HelloBaby/ViewController.swift
+++ Swift_Practice/lesson1_basicViewController/HelloBaby/HelloBaby/ViewController.swift
@@ -10,27 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    weak var label: UILabel!
-    weak var textfield: UITextField!
-    weak var button: UIButton!
-    func pressedButton(_ sender: Any){
-        label.text = textfield.text
-    }
+    var label: UILabel!
+    var textfield: UITextField!
+    var button: UIButton!
+//    func pressedButton(_ sender: Any){
+//        label.text = textfield.text
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        self.view.backgroundColor = UIColor.black;
         
+        label = UILabel(frame: CGRect(x: 20, y: 20, width: 200, height: 40))
+        label.layer.cornerRadius = 5.0
+        label.backgroundColor = UIColor.gray
+        label.text = "Hello World ?"
+        label.textAlignment = .center
         
-        label.frame.origin.x = 20
-        label.frame.origin.y = 20
+        textfield = UITextField(frame: CGRect(x: 20, y: 80, width: 200, height: 40))
+        textfield.layer.cornerRadius = 5.0
+        textfield.textAlignment = .center
+        textfield.textColor = UIColor.darkGray
+        textfield.backgroundColor = UIColor.gray
         
-        textfield.frame.origin.x = 20
-        textfield.frame.origin.y = 60
-        
-        button.frame.origin.x = 20
-        button.frame.origin.y = 100
+        // http://stackoverflow.com/questions/24102191/make-a-uibutton-programatically-in-swift
+        button = UIButton(frame: CGRect(x: 20, y: 140, width: 200, height: 40))
+        button.layer.cornerRadius = 5.0
+        button.backgroundColor = UIColor.gray
+        button.setTitle("確定", for: UIControlState.normal )
+        button.setTitleColor(UIColor.blue, for: UIControlState.normal )
+        button.addTarget(self,
+                         action:#selector(self.pressed) ,
+                         for:.touchUpInside)
         
         self.view.addSubview(label)
         self.view.addSubview(textfield)
@@ -42,6 +55,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK:
+    func pressed(sender: UIButton!){
+        label.text = textfield.text
+    }
+    
 }
 
