f974b8fc747b905f35d78a314117c80d3eaa499c
Coody <coody0829@gmail.com>
2017-03-31 10:23:08 +0800

    [ fix ] 將範例專案清空

--- Swift_Practice/lesson1_basicViewController/HelloBaby/HelloBaby/ViewController.swift
+++ Swift_Practice/lesson1_basicViewController/HelloBaby/HelloBaby/ViewController.swift
@@ -10,31 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    weak var label: UILabel!
-    weak var textfield: UITextField!
-    weak var button: UIButton!
-    func pressedButton(_ sender: Any){
-        label.text = textfield.text
-    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        
-        
-        label.frame.origin.x = 20
-        label.frame.origin.y = 20
-        
-        textfield.frame.origin.x = 20
-        textfield.frame.origin.y = 60
-        
-        button.frame.origin.x = 20
-        button.frame.origin.y = 100
-        
-        self.view.addSubview(label)
-        self.view.addSubview(textfield)
-        self.view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
