f66833bfc1d835681c3d8498318be1589587419a
Coody <coody0829@gmail.com>
2017-04-12 11:17:26 +0800

    [ feature ] 將範例改成標準 View & ViewController

---
+++ Swift_Practice/lesson1_basicViewController/HelloBaby/HelloBaby/Lesson2View.swift
@@ -0,0 +1,88 @@
+//
+//  Lesson2View.swift
+//  HelloBaby
+//
+//  Created by Chou Coody on 2017/4/2.
+//  Copyright © 2017年 Coody. All rights reserved.
+//
+
+import UIKit
+
+protocol Lesson2View_Protocol {
+    func pressedButton(sender:Any)
+}
+
+class Lesson2View : UIView , UITableViewDelegate , UITableViewDataSource {
+    
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return nil
+    }
+
+    
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    
+    var button: UIButton!
+    var classmateTableView: UITableView!
+    
+    var delegate:Lesson2View_Protocol?
+    
+    convenience init(_ frame: CGRect ){
+        self.init( frame: frame  )
+    }
+    
+    override init(frame: CGRect) {
+        super.init( frame:frame )
+        self.initialUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK: Public
+    func initialUI() -> Void {
+    
+        self.createUITableView()
+        self.createButton()
+        
+        self.addSubview(button)
+        
+        self.addAction()
+    }
+    
+    func addAction() -> Void {
+        self.button.addTarget(self,
+                              action:#selector(self.pressedButton(sender:)),
+                              for: UIControlEvents.touchUpInside)
+    }
+    
+    // MARK: initial    
+    private func createButton(){
+        button = UIButton.init(frame: CGRect(x: 0,
+                                             y:classmateTableView.frame.origin.y + classmateTableView.frame.size.height ,
+                                             width:self.frame.size.width,
+                                             height:45))
+        button.layer.cornerRadius = 5.0
+        button.backgroundColor = UIColor.gray
+        button.setTitle("確定", for: UIControlState.normal )
+        button.setTitleColor(UIColor.blue, for: UIControlState.normal )
+    }
+    
+    private func createUITableView(){
+        classmateTableView = UITableView.init(frame: CGRect(x: 0, y: 0, width:self.frame.size.width , height: self.frame.size.height - 45))
+        classmateTableView.delegate = self
+        classmateTableView.dataSource = self
+    }
+    
+    // MARK: Response
+    func pressedButton( sender:Any ){
+        delegate?.pressedButton( sender: sender )
+    }
+    
+}
---
+++ Swift_Practice/lesson1_basicViewController/HelloBaby/HelloBaby/Lesson2ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  Lesson2ViewController.swift
+//  HelloBaby
+//
+//  Created by Chou Coody on 2017/4/2.
+//  Copyright © 2017年 Coody. All rights reserved.
+//
+
+import UIKit
+
+class Lesson2ViewController : UIViewController{
+    
+    // MARK: init
+    var mainView:Lesson2View!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mainView = Lesson2View.init(frame: self.view.frame)
+        mainView.delegate = self;
+        self.view.addSubview(mainView)
+        
+        self.view.backgroundColor = UIColor.lightGray
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
+
+
+extension Lesson2ViewController : Lesson2View_Protocol{
+    
+    func pressedButton(sender: Any) {
+        self.mainView.label.text = self.mainView.textField.text
+    }
+    
+}
